78b6a46bbe7f7dc3bd383b4722f6245956d1b183
Nithin <nithinnitty56@gmail.com>
2026-02-07 13:31:22 +0530

    updated authroutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -10,7 +10,7 @@ console.log("🔥 AUTH ROUTES LOADED - send-otp available");
 const router = express.Router();
 
 router.post("/register", authMiddleware, registerUser);
-router.post("/send-Otp", sendOtp);
+router.post("/send-otp", sendOtp);
 router.post("/verify-otp", verifyOtp);
 
 router.get("/profile", authMiddleware, (req, res) => {
